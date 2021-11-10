e9a613c8d6844377de053842c8a44185c637b9f9
Tien <tienle676@gmail.com>
2021-11-10 20:29:03 +0700

    add [send mail] api

--- user/views.py
+++ user/views.py
@@ -5,6 +5,23 @@ from rest_framework import serializers, viewsets, permissions
 from django.contrib.auth import authenticate
 from .serializers import LoginSerializer
 
+from django.conf import settings
+from django.core.mail import send_mail
+import re
+ 
+# Make a regular expression
+# for validating an Email
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+  
+def check_email(email):
+ 
+    # pass the regular expression
+    # and the string into the fullmatch() method
+    if(re.fullmatch(regex, email)):
+        return True
+ 
+    else:
+        return False
 
 class UserViewSet(viewsets.ViewSet):
     
@@ -19,3 +36,20 @@ class UserViewSet(viewsets.ViewSet):
             serializer = LoginSerializer(user)
             return Response(serializer.data)
         return Response({ 'error': 'wrong password or username' })
+
+    @action(detail=False, methods=['post'])
+    def send_email(self, request):
+        from_email = request.data.get("from_email", None)
+        text       = request.data.get("text", None)
+
+        if from_email and text and check_email(from_email):
+            
+            subject = 'PORTIFLO MAIL'
+            message = f'From email customer {from_email}, content: {text}'
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = [settings.EMAIL_HOST_USER, ]
+            send_mail( subject, message, email_from, recipient_list )
+
+            return Response('sent')
+
+        return Response({ 'error': 'invalid field' })
